8882235a7997fa64ef0e74de09404c140db937cc
Thomas J. Lauer <tlauer2@gustavus.edu>
2022-02-09 18:47:25 -0600

    Create CollapsableDiv.js

---
+++ src/CollapsableDiv.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+/**
+ * create a div that can be clicked on to show or hide its children
+ * @param {string} legendTitle title of the div
+ * @param {boolean} keep whether the children should be kept in
+ * the DOM when hidden. Recommended true (the default) for everything
+ * except very expensive divs that change constantly even when closed
+ * @returns {JSX.Element} rendering
+ */
+export function CollapsableDiv({ title, children, keep = true }) {
+  const [visible, setVisible] = useState(true);
+
+  /* toggle visibilty state */
+  const onClick = () => {
+    setVisible(!visible);
+  };
+
+  return (
+    <>
+      <div onClick={onClick} className="menu_button">{title}</div>
+      <div className={visible ? "hidden" : ""}>
+        {(!keep && !visible) ? null : children}
+      </div>
+    </>
+  );
+}
