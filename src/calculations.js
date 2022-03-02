5c8361bcd2571a0c628914b55df79197cc43e5a0
Thomas J. Lauer <tlauer2@gustavus.edu>
2022-03-02 17:03:28 -0600

    Remove "getCalculatedValues"

--- src/calculations.js
+++ src/calculations.js
@@ -1,11 +1,3 @@
-//import { useEffect } from 'react';
-
-function compDeps(statePrev, state, vars) {
-  let varsLength = vars.length;
-  for(let i = 0; i < varsLength; i++) { if(state[vars[i]] !== statePrev[vars[i]]) { return true; } }
-  return false;
-}
-
 /**
  * calculate total porosity
  * @param {number} epsilonE interstitual porosity
@@ -18,18 +10,4 @@ export function calcEpsilonTotal(epsilonE, epsilonI) {
 
 export function calcVoidVolume(innerDiameter, length, epsilonT) {
   return Math.PI*Math.pow((innerDiameter/2),2)*length*epsilonT;
-}
-
-export function getCalculatedValues(statePrev, state) {
-  if(compDeps(statePrev, state, ["epsilonI", "epsilonE"])) {
-    state.epsilonT = calcEpsilonTotal(state.epsilonE, state.epsilonI);
-    console.log("epsilonT: "+state.epsilonT);
-  }
-
-  if(compDeps(statePrev, state, ["innerDiameter","length","epsilonT"])) {
-    state.voidVolume = calcVoidVolume(state.innerDiameter, state.length, state.epsilonT);
-    console.log("voidVolume: "+state.voidVolume);
-  }
-
-  return state;
 }
