24cfefde9da428e13cd4c739cdb7670d559e11dd
Thomas J. Lauer <tlauer2@gustavus.edu>
2022-03-02 17:30:55 -0600

    Update calculations.js

--- src/calculations.js
+++ src/calculations.js
@@ -5,9 +5,22 @@
  * @returns total porosity
  */
 export function calcEpsilonTotal(epsilonE, epsilonI) {
-  return epsilonE + epsilonI * (1 - epsilonE);
+  const epsilonT = epsilonE + epsilonI * (1 - epsilonE);
+  console.debug(`epsilonT: ${epsilonT}`);
+  return epsilonT;
 }
 
-export function calcVoidVolume(innerDiameter, length, epsilonT) {
-  return Math.PI*Math.pow((innerDiameter/2),2)*length*epsilonT;
+export function calcVoidVolume(innerDiameter_mm, length_mm, epsilonT) {
+  const innerDiameter_cm = innerDiameter_mm / 10;
+  const length_cm = length_mm / 10;
+  const voidVolume = Math.PI*Math.pow((innerDiameter_cm/2),2)*length_cm*epsilonT;
+  console.debug(`voidVolume: ${voidVolume}`);
+  return voidVolume;
+}
+
+export function calcVoidTime(voidVolume_mL, flowRate_mL_min) {
+  const voidTime_min = voidVolume_mL / flowRate_mL_min;
+  const voidTime_sec = voidTime_min * 60;
+  console.debug(`voidTime: ${voidTime_sec}`);
+  return voidTime_sec;
 }
