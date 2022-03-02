dfb729a70bc0ace71aa8c9e8a807d42724ea1509
Thomas J. Lauer <tlauer2@gustavus.edu>
2022-03-02 16:33:25 -0600

    Update calculations.js

--- src/calculations.js
+++ src/calculations.js
@@ -1,3 +1,11 @@
+//import { useEffect } from 'react';
+
+function compDeps(statePrev, state, vars) {
+  let varsLength = vars.length;
+  for(let i = 0; i < varsLength; i++) { if(state[vars[i]] !== statePrev[vars[i]]) { return true; } }
+  return false;
+}
+
 /**
  * calculate total porosity
  * @param {number} epsilonE interstitual porosity
@@ -6,4 +14,22 @@
  */
 export function calcEpsilonTotal(epsilonE, epsilonI) {
   return epsilonE + epsilonI * (1 - epsilonE);
+}
+
+export function calcVoidVolume(innerDiameter, length, epsilonT) {
+  return Math.PI*Math.pow((innerDiameter/2),2)*length*epsilonT;
+}
+
+export function getCalculatedValues(statePrev, state) {
+  if(compDeps(statePrev, state, ["epsilonI", "epsilonE"])) {
+    state.epsilonT = calcEpsilonTotal(state.epsilonE, state.epsilonI);
+    console.log("epsilonT: "+state.epsilonT);
+  }
+
+  if(compDeps(statePrev, state, ["innerDiameter","length","epsilonT"])) {
+    state.voidVolume = calcVoidVolume(state.innerDiameter, state.length, state.epsilonT);
+    console.log("voidVolume: "+state.voidVolume);
+  }
+
+  return state;
 }
