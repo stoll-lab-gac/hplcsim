05620875b5c08d209605e8186ae54f85d1bdf806
Thomas J. Lauer <tlauer2@gustavus.edu>
2022-03-04 17:20:20 -0600

    Include compoundParameters in state

--- src/Provider.js
+++ src/Provider.js
@@ -2,13 +2,46 @@ import { useReducer } from 'react';
 import { App } from './App'
 //import { version } from '../package.json';
 
+import { compoundParameters, M } from './CompoundParameters'
+
 function init(){
 
+  let compoundParametersNew = compoundParameters;
+  let allSolventBs = [];
+  const columns = Object.keys(compoundParametersNew);
+  for(let i = 0; i < columns.length; i++) {
+    const solventBs = Object.keys(compoundParametersNew[columns[i]]);
+    for(let j = 0; j < solventBs.length; j++) {
+      allSolventBs.push(solventBs[j]);
+      const compounds = Object.keys(compoundParametersNew[columns[i]][solventBs[j]]);
+      for(let k = 0; k < compounds.length; k++) {
+        if(!Object.keys(compoundParametersNew[columns[i]][solventBs[j]][compounds[k]]).includes("M")) {
+          const M_Indx = Math.floor(Math.random()*(M.length-1));
+          compoundParametersNew[columns[i]][solventBs[j]][compounds[k]]["M"] = M[M_Indx];
+        }
+      }
+    }
+  }
+
+  const allSolventAs_unique = ['Water'];
+  const allSolventBs_unique = [...new Set(allSolventBs.map(item => item))];
+
+  const stateCompounds = {
+    "compoundParameters": compoundParametersNew,
+    "compoundList": [
+      "phenol",
+      "benzonitrile",
+      "p-chlorophenol",
+      "acetophenone",
+      "nitrobenzene"
+    ]
+  };
+
   const stateInputs = {
-    solventAs: ['Water','Buffer, 3.2 pH'],
-    solventBs: ['Acetonitrile', 'Methanol'],
-    solventA: 'Water',
-    solventB: 'Acetonitrile',      // ACN or MeOH
+    solventAs: allSolventAs_unique, //['Water'], //['Water','Buffer, 3.2 pH'],
+    solventBs: allSolventBs_unique, //['Acetonitrile', 'Methanol'],
+    solventA: allSolventAs_unique[0], //'Water',
+    solventB: allSolventBs_unique[0], //'Acetonitrile',      // ACN or MeOH
     useGradient: false,     // to use gradient or not
     phi0: 0.4,              // initial phi [0-1] 
     phiFinal: 0.4,          // final phi [0-1]
@@ -18,8 +51,8 @@ function init(){
     injectionVolume: 5,     // injection volume (microliters)
     flowRate: 2.0,          // flow rate (mL/min)
     
-    columns: ['Agilent SB-C18'],
-    selectedColumn: 'Agilent SB-C18',
+    columns: columns, //['Agilent SB-C18'],
+    selectedColumn: columns[0], //'Agilent SB-C18',
     length: 100.0,
     innerDiameter: 4.6,
     particleSize: 3.0,
@@ -41,7 +74,11 @@ function init(){
     flowVelocity_reduced: 0,
     diffusionCoefficient: 0.000011689,
   };
-  const state = {...stateInputs, ...stateOutputs};
+
+  const state = {...stateCompounds, ...stateInputs, ...stateOutputs};
+
+  console.log(state);
+  
   return state;
 }
 
