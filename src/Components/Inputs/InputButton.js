cff0f16bc1258a282a93407d94f909304f69cf2c
Thomas J. Lauer <tlauer2@gustavus.edu>
2022-03-09 10:47:36 -0600

    Create InputButton.js

---
+++ src/Components/Inputs/InputButton.js
@@ -0,0 +1,22 @@
+export function InputButton({
+  id,
+  value,
+  className,
+  onClick,
+  disabled=false
+}) {
+
+  return (
+    <>
+    <input
+      type="button"
+      id={id}
+      className={className}
+      value={value}
+      onClick={onClick}
+      disabled={disabled}
+    />
+    </>
+  );
+
+}
