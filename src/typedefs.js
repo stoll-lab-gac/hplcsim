d19e836b3ac3226b215cb1e17b43d656e09cbba7
Thomas J. Lauer <tlauer2@gustavus.edu>
2022-03-24 14:05:50 -0500

    Create "src/typedefs.js"

---
+++ src/typedefs.js
@@ -0,0 +1,139 @@
+/**
+ * A number that has no units i.e. units canceled out
+ * @typedef {number} dimensionless
+ * @author Thomas J. Lauer
+ * @since 17 March 2022
+ */
+
+/**
+ * A number containing a Pressure in Bar
+ * @typedef {number} pressure_bar
+ * @author Thomas J. Lauer
+ * @since 18 March 2022
+ */
+
+/**
+ * A number containing a Molecular Weight in Grams per Mol
+ * @typedef {number} molecularWeight_g_mol
+ * @author Thomas J. Lauer
+ * @since 18 March 2022
+ */
+
+/**
+ * A number containing a Time in Seconds
+ * @typedef {number} time_s
+ * @author Thomas J. Lauer
+ * @since 17 March 2022
+ */
+
+/**
+ * A number containing a Time in Minutes
+ * @typedef {number} time_min
+ * @author Thomas J. Lauer
+ * @since 17 March 2022
+ */
+
+/**
+ * A number containing a Temperature in Celcius
+ * @typedef {number} temperature_C
+ * @author Thomas J. Lauer
+ * @since 17 March 2022
+ */
+
+/**
+ * A number containing a Temperature in Kelvin
+ * @typedef {number} temperature_K
+ * @author Thomas J. Lauer
+ * @since 17 March 2022
+ */
+
+/**
+ * A number containing a Temperature in Fahrenheit
+ * @typedef {number} temperature_F
+ * @author Thomas J. Lauer
+ * @since 17 March 2022
+ */
+
+/**
+ * A number containing a Viscosity in Centipoise
+ * @typedef {number} viscosity_cP
+ * @author Thomas J. Lauer
+ * @since 17 March 2022
+ */
+
+/**
+ * A number containing a Volume in Milliliters
+ * @typedef {number} volume_mL
+ * @author Thomas J. Lauer
+ * @since 17 March 2022
+ */
+
+/**
+ * A number containing a Volume in Microliters
+ * @typedef {number} volume_uL
+ * @author Thomas J. Lauer
+ * @since 17 March 2022
+ */
+
+/**
+ * A number containing a Length in Micrometers
+ * @typedef {number} length_um
+ * @author Thomas J. Lauer
+ * @since 17 March 2022
+ */
+
+/**
+ * A number containing a Length in Millimeters
+ * @typedef {number} length_mm
+ * @author Thomas J. Lauer
+ * @since 17 March 2022
+ */
+
+/**
+ * A number containing a Length in Centimeters
+ * @typedef {number} length_cm
+ * @author Thomas J. Lauer
+ * @since 17 March 2022
+ */
+
+/**
+ * A number containing a Length in Ceters
+ * @typedef {number} length_m
+ * @author Thomas J. Lauer
+ * @since 17 March 2022
+ */
+
+/**
+ * A number containing an Area in Square Centimeters
+ * @typedef {number} area_cm2
+ * @author Thomas J. Lauer
+ * @since 17 March 2022
+ */
+
+/**
+ * A number containing a Velocity in Centimeters per Second
+ * @typedef {number} velocity_cm_s
+ * @author Thomas J. Lauer
+ * @since 17 March 2022
+ */
+
+/**
+ * A number containing a Volumetric Flow Rate in Milliliters per Minute
+ * @typedef {number} flowRate_mL_min
+ * @author Thomas J. Lauer
+ * @since 17 March 2022
+ */
+
+/**
+ * A number containing an Acceleration in Square Centimeters per Second
+ * @typedef {number} acceleration_cm2_sec
+ * @author Thomas J. Lauer
+ * @since 17 March 2022
+ */
+
+/**
+ * A number ranging from 0-1 inclusive
+ * @typedef {number} percent
+ * @author Thomas J. Lauer
+ * @since 17 March 2022
+ */
